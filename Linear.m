c4edae152650f0eed9eb0f3e362034806047b1b2
Qargwic <invoo@bk.ru>
2017-03-18 21:44:59 +0300

    Доработки линеаризации

--- Linear.m
+++ Linear.m
@@ -27,8 +27,16 @@ classdef Linear < Given
         end
     end
     
-    methods (Access = private)
-
+    methods (Static)
+        function [y,x]=lmsim(a,b,c,d,t,x0,u)
+            h=t(2)-t(1);
+            [m,n] = size(a);
+            [m,nb] = size(b);
+            s = expm([[a b]*h; zeros(nb,n+nb)]);
+            p = s(1:n,1:n);
+            g= s(1:n,n+1:n+nb);
+            x=(ltitr(p,g,u',x0'))';
+            y=c*x+d*u;
     end
     
 end
