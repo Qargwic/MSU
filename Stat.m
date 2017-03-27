b34700d6bd1919c8936fe681b6247c8f987278c9
Qargwic <invoo@bk.ru>
2017-03-27 18:53:46 +0300

    Исправление матриц в статической системе

--- Stat.m
+++ Stat.m
@@ -11,12 +11,12 @@ classdef Stat < Given
         
         function z = fun_F(obj, x, U)
             z = [1/(obj.W*obj.Ph)*(U(1)*obj.Uc_n-obj.i_n*(obj.R_ya+obj.R_v)*polyval(obj.p, x(1))-obj.Ce*x(2)*obj.w_n*x(1)*obj.Ph);
-                1/(obj.J*obj.w_n)*(obj.Cm*obj.Ph*obj.i_n*(p(1)*x(1)^4+p(3)*x(1)^2)-U(2)*obj.Mc_n)];
+                1/(obj.J*obj.w_n)*(obj.Cm*obj.Ph*obj.i_n*polyval([obj.p 0], x(1))-U(2)*obj.Mc_n)];
         end
         
         function z = fun_G(obj, x, U)
-            z = [-1/(obj.W*obj.Ph)*(obj.i_n*(obj.R_ya+obj.R_v)*(p(1)*3*x(1)^2+p(3))+obj.Ce*x(2)*obj.w_n*x(1)*obj.Ph) -obj.Ce*obj.w_n*obj.Ph/(obj.W*obj.Ph)*x(1);
-                obj.Cm*obj.Ph*obj.i_n/(obj.J*obj.w_n)*(obj.p(1)*4*x(1)^3+obj.p(3)*2*x(1)) 0];
+            z = [-1/(obj.W*obj.Ph)*(obj.i_n*(obj.R_ya+obj.R_v)*Given.dpol(obj.p, x(1))+obj.Ce*x(2)*obj.w_n*x(1)*obj.Ph) -obj.Ce*obj.w_n*obj.Ph/(obj.W*obj.Ph)*x(1);
+                obj.Cm*obj.Ph*obj.i_n/(obj.J*obj.w_n)*Given.dpol([obj.p 0], x(1)) 0];
         end
         
         function [xx, uu] = graph(obj, Pref)
