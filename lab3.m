b3a4fe9386057a8dc1d4d7cfa94397104e23fea9
Qargwic <invoo@bk.ru>
2017-03-27 21:11:49 +0300

    Lab3

--- lab3.m
+++ lab3.m
@@ -1,8 +1,20 @@
-p = polinom(3);
-p = p(1,:);
-D = Dynamic(p);
-P = Preference;
-G = Graphic;
-[xx, yy, t] = D.graph(P, [1 1.2]);
-G.setGs(t, xx);
-G.draw(1);
+cass={[1 1];[1 0.8];[1 1.2];[0.8 1];[1.2 1];[0.8 0.8];[1.2 1.2]}; 
+p = polinom(3); 
+p = p(1,:); 
+D = Dynamic(p); 
+P = Preference; 
+G = Graphic; 
+P.X0=[0; 0]; 
+for i=1:7 
+    figure() 
+    [xx, yy, t] = D.graph(P, cass{i}); 
+    G.setGs(t,xx) 
+    G.setG(t,yy(1,:)); 
+    G.setG(xx(1,:),xx(2,:)) 
+    G.setNs('Переходный процесс x1(t)',...
+        'Переходный процесс x2(t)',...
+        'Переходный процесс y(t)',...
+        'Фазовый портрет x1(x2)') 
+    G.draw(1); 
+    G.clear; 
+end
